8de9017bc07aa2c48a83abdc9ec8f1b69dd865f4
Jmtrw Gzfrjz <121946252+tzjkm@users.noreply.github.com>
2025-10-13 15:16:04 +0530

    Getting prepared for github pages deployment

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ const isProd = process.env.NODE_ENV === "production";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+	output: "export",
 	basePath: isProd ? "/ecommerce-application" : "",
 	assetPrefix: isProd ? "/ecommerce-applicaton/" : "",
 };
